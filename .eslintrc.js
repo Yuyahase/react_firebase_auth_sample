69f8819af8bb980dc98726542955e7606c59a0be
haseyuy <haseyuy@gmail.com>
2021-06-13 21:58:31 +0900

    lint

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+// ESLint は JavaScript のための静的検証ツール
+//js, YAML, JSON形式で記述できる
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      }
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      }
+    ],
+    'react/react-in-jsx-scope': 'off',
+    "import/no-unresolved": "off",
+    "no-use-before-define": "off",
+  }
+}
